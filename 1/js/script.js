77f8c8e352cab3039b9dd4b96208682c55bbc7ef
Donadonzi <donadonzi@gmail.com>
2020-03-31 23:37:56 -0400

    adding section2 to jQuery repo

---
+++ 1/js/script.js
@@ -0,0 +1,72 @@
+$(function() {
+  
+	// $(".red-box").toggle(2000),
+	// // $(".green-box").fadeTo(3000, .5),
+	// // $(".blue-box").fadeTo(4000, .8),
+	// $(".green-box").fadeToggle()
+	// $(".green-box").slideUp(1000),
+	// $(".green-box").slideDown(2000)
+	// $('p').hide();
+	// $('p').slideDown(2000);
+	// $('p').animate({
+	// 	fontSize: '2rem'
+	// }, 1000);
+	// $('.green-box').animate({
+	// 	'margin-right': '+=20rem'
+	// }, 2000);
+	// $('.green-box').animate({
+	// 	'margin-right': '-=20rem'
+	// }, 2000, 'linear', function() {
+	// 	// alert("I'm done!");
+	// 	$('.red-box').animate({
+	// 		marginRight: '20rem'
+	// 	}, function() {
+	// 		$('.green-box').slideUp(1000);
+	// 	});
+	// });
+
+	// $('.lightbox').delay(5000).fadeIn(1000);
+
+	// $('p').css('color', 'blue'),
+	// $('input[type="email"]').css('background-color', 'yellow')
+
+	// $('h2:first').css('color', 'green'),
+	// // $('li:last-child').css('color', 'green')
+	// $('li:last').css('color', 'green'),
+	// $('input:text').css('background-color', 'red')
+
+	// $('#list').find('ul').css('background-color', 'red')
+	// $('#list').children('li').css('background-color', 'red')
+	// $('#test').next().css('background-color', 'red')
+	// $(':header').next().css('background-color', 'yellow'),
+	// $('form').children(':password').css('background-color', 'red')
+
+	// $('#list').find('li').filter(':even').css('background-color', 'yellow')
+	// $('#list').find('li').filter(function(index) { 
+	// 	return index === 4;
+	// }).css('background-color', 'yellow'),
+	// $('li').filter(function(index) { 
+	// 	return index === 4;
+	// }).css('background-color', 'yellow')
+	// $('li').eq(-4).css('background-color', 'purple'),
+	// $('li').eq(2).css('background-color', 'purple'),
+	// $('li').not('#list ul li').css('background-color', 'blue')
+
+	// $('#list li:first ul').append('<li>Hey!</li>'),
+	// $('<li>Hey again!</li>').appendTo($('#list li:last')),
+	// $('<h5>Donya</h5>').prependTo('h1'),
+	// $('.blue-box').after('<div class="blue-box">I am new!</div>'),
+	// $('.green-box').before( function() {
+	// 	return $('<div class="green-box">I am being added via function</div>')
+	// });
+
+	// var greenBox = $('<div class="green-box">I am new!!</div>');
+	// $('.red-box, .blue-box').replaceWith(greenBox);
+	// $(greenBox).replaceAll('.red-box, .blue-box');
+
+	// $('form').children().not("textarea, input:text, br").remove();
+	// $('form').empty();
+	// $('p:first').empty();
+
+	
+});
