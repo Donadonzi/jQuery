30b7d4cb5f2aa322e74a3b05e1baf270164725d4
Donadonzi <donadonzi@gmail.com>
2020-04-02 02:07:35 -0400

    event handling part I

--- 1/js/script.js
+++ 1/js/script.js
@@ -98,34 +98,126 @@ $(function() {
 	// $('.gallery').css('display', 'none');
 
 // =========================== CHANGING CSS =================================
-	$('.red-box').css('height', '+=2rem');
-	const properties = $('p').css(['font-size', 'display', 'color']);
-	console.log(properties);
-	$('.green-box').css('user-select', 'none');
+	// $('.red-box').css('height', '+=2rem');
+	// const properties = $('p').css(['font-size', 'display', 'color']);
+	// console.log(properties);
+	// $('.green-box').css('user-select', 'none');
 
-	$('a').addClass('fancy-link');
-	$('div').addClass( function(i, cur){
-		if (cur === 'dummy') {
-			return 'red-box';
+	// $('a').addClass('fancy-link');
+	// $('div').addClass( function(i, cur){
+	// 	if (cur === 'dummy') {
+	// 		return 'red-box';
+	// 	}
+	// });
+
+// =========================== ADD DATA TO AN ELEMENT =================================
+// const gallery = $('.gallery');
+
+// var sources = [
+// 	"./images/laptop-mobile_small.jpg",
+// 	"./images/laptop-on-table_small.jpg",
+// 	"./images/people-office-group-team_small.jpg"
+// ];
+
+// gallery.data('availableImages', sources);
+// console.log(gallery.data('availableImages'));
+// gallery.data('name', 'My-Gallery');
+// console.log(gallery.data());
+
+// // =========================== CONTENT OF AN ELEMENT =================================
+// const markUpText = $('p:first').html();
+// $('p:first').html(markUpText + '<strong>I have been added here via jQuery</strong>');
+	
+
+// =========================== EVENT HANDLING =================================
+// $('.btn-dummy').click(function(e){
+// 	console.log(e);
+// });
+// $('.red-box').click(function(){
+// 	$(this).fadeTo(1000, .5);
+// });
+// $('.green-box').hover(function() {
+// 	$(this).text('I am hovered!');
+// 	$(this).animate({'margin-right': '20rem'}, 1000);
+// });
+// $('.blue-box').hover(function() {
+// 	$(this).stop().fadeTo(500, .5);
+// }, function() {
+// 	$(this).stop().fadeTo(500, 1);
+// });
+
+//=== Gallery with click event ====
+// var sources = [
+// 		"./images/laptop-mobile_small.jpg",
+// 		"./images/laptop-on-table_small.jpg",
+// 		"./images/people-office-group-team_small.jpg"
+// 	];
+
+// 	var image = $('.gallery').find('img').first();
+// 	var i = 0;
+
+	// image.on("click", function() {
+	// 	i = (i+1) % sources.length;
+	// 	$(this).fadeOut(function(){
+	// 		image.attr('src', sources[i]).fadeIn();
+	// 	});	
+	// });
+
+	//=== Modularizing the above procedure by taking out the anonymous function ====
+	// function nextImage() {
+	// 	i = (i + 1) % sources.length;
+	// 	image.fadeOut(function() {
+	// 		image.attr('src', sources[i]).fadeIn();
+	// 	});
+	// }
+	
+	// image.on('click', nextImage);
+
+//=== Event delegation ====	
+	// $('#list').append('<li>I am an appended element</li>');
+	// $('body').on('mouseenter', 'li', function() {
+	// 	$(this).css('color', 'cyan');
+	// });
+	// $('body').on('mouseleave', 'li', function() {
+	// 	$(this).css('color', 'black');
+	// });
+
+//=== Adding additional data to the event ====	
+	// $('.btn-dummy').click({
+	// 	user: 'Donya'
+	// }, function(e){
+	// 	alert('Hi' + e.data.user);
+	// });
+
+// =========================== IMAGE GALLERY WITH LIGHTBOX PREVIEW=================================
+	const images = $('.gallery').find('img');
+	images.css('width', '33%').css('opacity', '.7');
+
+	images.mouseenter(function(){ 
+		$(this).stop().fadeTo(200, 1);
+	});
+
+	images.mouseleave(function() {
+		$(this).stop().fadeTo(200, .7);
+	});
+
+	images.click(function(){
+		const source = $(this).attr('src');
+		const image = $('<img>').attr('src', source).css('width', '150%');
+		$('.lightbox').empty().append(image).fadeIn();
+	});
+
+	$('.lightbox').click(function(){
+		$(this).stop().fadeOut(200);
+	});
+	
+// =========================== HANDLIGN KEYDOWN AND KEYUP EVENTS =================================
+	$('html').keydown(function(e){
+		console.log(e.which);
+		if (e.which === 39) {
+			$('.blue-box').stop().css('margin-left', '+=1rem');
 		}
 	});
 
-// =========================== ADD DATA TO AN ELEMENT =================================
-const gallery = $('.gallery');
-
-var sources = [
-	"./images/laptop-mobile_small.jpg",
-	"./images/laptop-on-table_small.jpg",
-	"./images/people-office-group-team_small.jpg"
-];
-
-gallery.data('availableImages', sources);
-console.log(gallery.data('availableImages'));
-gallery.data('name', 'My-Gallery');
-console.log(gallery.data());
-
-// =========================== CONTENT OF AN ELEMENT =================================
-const markUpText = $('p:first').html();
-$('p:first').html(markUpText + '<strong>I have been added here via jQuery</strong>');
-	
 });
+
