bbdf3e223c5a0f8e9ae0bbd76e80e458c8ef6217
Donadonzi <donadonzi@gmail.com>
2020-04-06 15:14:11 -0400

    building the memory game

--- jQueryInAction/script.js
+++ jQueryInAction/script.js
@@ -1,23 +1,93 @@
-var interval;
-    function startTicker()
-    {
-        $("#news li:first").slideUp(function(){
-            $(this).appendTo($("#news")).slideDown();
-        });
-    }
-    
-    function stopTicker()
-    {
-        clearInterval(interval);
-    }
-    
-    $(document).ready(function(){
-        startTicker();
-        interval = setInterval(startTicker, 3000);
-        $("#news").hover(function(){
-                stopTicker();
-             }, function(){
-                    startTicker();
-                    interval = setInterval(startTicker, 3000);
-        });
-    });
+var imgSource = "";
+	var divId = "";
+	var imgFound = 0;
+	var moves = 0;
+	var imageArray = [
+					  "images/flo1.jfif",
+					  "images/flo2.jfif",
+					  "images/flo3.jfif",
+					  "images/flo4.jfif",
+					  "images/flo5.jfif",
+					  "images/flo6.jfif",
+					  "images/flo7.jfif",
+					  "images/flo8.jfif",
+					  "images/flo9.jfif",
+					  "images/flo10.jfif",
+					  ];
+	function ShuffleImages()
+	{
+		var len = imageArray.length*2;
+		var imgArr = new Array();
+		imgArr = $.merge($.merge([], imageArray), imageArray);
+		var currentDiv = $("#images div:first-child");
+		for(var z = 0;z < len;z++)
+		{
+			var randomNum = Math.round(Math.random()*(imgArr.length-1));
+			$("#" + currentDiv.attr("id") + " img").attr("src", imgArr[randomNum]);
+			imgArr.splice(randomNum, 1);
+			currentDiv = currentDiv.next();
+		}
+	}
+	
+	function OpenCard()
+	{
+		var id = $(this).attr("id");
+		if ($("#" + id + " img").is(":hidden"))
+		{
+			$("#" + id + " img").show("slow");
+			if (imgSource == "")
+			{
+				divId = id;
+			  imgSource = $("#" + id + " img").attr("src");
+			}
+			else
+			{
+				currentOpened = $("#" + id + " img").attr("src");
+				if (imgSource != currentOpened)
+				{
+					setTimeout(function(){
+							$("#" + id + " img").hide("slow");
+							$("#" + divId + " img").hide("slow");
+							divId = "";
+					        imgSource = "";
+						}, 600);
+					
+				}
+				else
+				{
+					divId = "";
+					imgSource = "";
+					imgFound++;
+				}
+			}
+			moves++;
+			$("#moves").html(moves);
+			if (imageArray.length == imgFound)
+			{
+				setTimeout(function(){
+					alert("YES !!! You completed the game with " + moves + " clicks");
+				}, 1000);
+			}
+		}
+	}
+	
+	function ResetGame()
+	{
+		ShuffleImages();
+		moves = 0;
+		$("#moves").html(moves);
+		$("#images div img").hide();
+		imgFound = 0;
+		imgSource = "";
+		divId = "";
+	}
+	$(document).ready(function(){
+		for(var i=1;i<3;i++)
+		{
+			$.each(imageArray, function(index, value){
+				$("#images").append("<div id=card" + i + index + "><img src=" + value + " /></div>");
+			})
+		}
+		ShuffleImages();
+		$("#images div").click(OpenCard);
+	});
