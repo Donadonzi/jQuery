ff99dd2bbe547a3c6ffbf0a60384eb0745784df5
Donadonzi <donadonzi@gmail.com>
2020-04-06 23:53:08 -0400

    making a simple animated tooltip

--- jQueryInAction/script.js
+++ jQueryInAction/script.js
@@ -1,18 +1,10 @@
-function displayGraph()
-		{
-			$("tbody tr").each(function(index){
-				$(".column").css("height", "0");
-				var cell = $(this).children("td").eq(1).text();
-				$("#column" + index).animate({height:cell}, 1500).html("<div>" + cell + "</div>")
-			});
-		}
-		$(document).ready(function(){
-			var arr = [25, 90, 60, 53];
-			var cells = $("tr td");
-			for(var i=1, j=0;i<cells.length;i+=3)
-			{
-				cells[i].innerHTML = arr[j] + "%";
-				j++;
-			}
-			displayGraph();
-		});
+$(document).ready(function(){
+	$("a").append("<span class='tooltip'></span>");
+	$("a").hover(function(){
+			$(this).find("span").fadeIn(2000);
+			var capital = $(this).attr("title");
+			$(this).find("span").text(capital);
+		}, function(){
+			$(this).find("span").fadeOut(1000);
+	});
+});
