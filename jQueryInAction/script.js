00e53fedd5d0a3a3ae8e9be2a393227318a83426
Donadonzi <donadonzi@gmail.com>
2020-04-09 20:04:19 -0400

    making a multi-step form  with validation

--- jQueryInAction/script.js
+++ jQueryInAction/script.js
@@ -1,20 +1,49 @@
 $(document).ready(function(){
-	$("li").mouseover(function(){
-			var current = $(this);
-			$("li").each(function(index){
-					$(this).addClass("hovered-stars");
-					if(index == current.index())
+	function doValidation(items)
+	{
+			var empty = 0;
+			$.each(items, function(key, item){
+					//console.log(key + " : " + $(item));
+					if($(item).prop("tagName") == "INPUT" && item.value == "")
 					{
-							return false;
+							empty++;
 					}
 			});
+			return empty;
+	}
+	$(".next").click(function(e){
+			e.preventDefault();
+			var elements = $(this).parent().children();
+			var error = doValidation(elements);
+			if(error == 0)
+			{
+					$(".individual").css("display", "none");
+					var nextDiv = $(this).parent().next().attr("id");  
+					$("#" + nextDiv).fadeIn();
+					var num = $(this).data("num");
+					$("#bar" + num + ", #icon" + num).addClass("active");
+			}
+			else
+			{
+					alert("Fill all the details.");
+			}
+			
 	});
-	$("li").mouseleave(function(){
-			$("li").removeClass("hovered-stars");
+	$(".previous").click(function(e){
+			e.preventDefault();
+			$(".individual").css("display", "none");
+			var prevDiv = $(this).parent().prev().attr("id");
+			$("#" + prevDiv).fadeIn();
+			var prevNum = $(this).data("num");
+			$("#bar" + prevNum + ", #icon" + prevNum).removeClass("active");
 	});
-	$("li").click(function(){
-			$("li").removeClass("clicked-stars");
-			$(".hovered-stars").addClass("clicked-stars");
-			$("#message").html("Thanks! You have rated this " + $(".clicked-stars").length + " star(s).")
+	$("input[type=submit]").click(function(){
+			var siblings = $(this).parent().children();
+			var errors = doValidation(siblings);
+			if(errors != 0)
+			{
+					alert("Fill all the details.");
+					return false;
+			}
 	});
 });
